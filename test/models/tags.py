4c6c56644774c38ab4de61f7003fc05ea7a1993b
Tho Ha <4588986+ThoDHa@users.noreply.github.com>
2025-03-10 07:08:12 -0400

    Updating Tags Model to match new API

--- test/models/tags.py
+++ test/models/tags.py
@@ -21,15 +21,19 @@ class TagsModelTest(unittest.TestCase):
     def test_valid(self):
         json = {
             "id": 1,
+            "device_id": 12,
             "name": "Valid Tag",
             "description": "This is a valid tag",
+            "data_type": "float",
             "range_low": 0.2,
             "range_high": 0.8,
         }
         tag = Tag(
             id=1,
+            device_id=12,
             name="Valid Tag",
             description="This is a valid tag",
+            data_type="float",
             range_low=0.2,
             range_high=0.8,
         )
@@ -44,8 +48,10 @@ class TagsModelTest(unittest.TestCase):
     def test_invalid_extra_value(self):
         json = {
             "id": 1,
+            "device_id": 12,
             "name": "Valid Tag",
             "description": "This is a valid tag",
+            "data_type": "float",
             "range_low": 0.2,
             "range_high": 0.8,
             "extra": 1,
@@ -55,8 +61,22 @@ class TagsModelTest(unittest.TestCase):
 
     def test_invalid_id_missing(self):
         json = {
+            "device_id": 12,
             "name": "Valid Tag",
             "description": "This is a valid tag",
+            "data_type": "float",
+            "range_low": 0.2,
+            "range_high": 0.8,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_device_id_missing(self):
+        json = {
+            "id": 1,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "float",
             "range_low": 0.2,
             "range_high": 0.8,
         }
@@ -66,8 +86,10 @@ class TagsModelTest(unittest.TestCase):
     def test_invalid_id_string(self):
         json = {
             "id": "TEST_STRING",
+            "device_id": 12,
             "name": "Valid Tag",
-            "description": "This is a valid tag",
+            "description": "This is a invalid tag",
+            "data_type": "float",
             "range_low": 0.2,
             "range_high": 0.8,
         }
@@ -77,7 +99,9 @@ class TagsModelTest(unittest.TestCase):
     def test_invalid_name_missing(self):
         json = {
             "id": 1,
-            "description": "This is a valid tag",
+            "device_id": 12,
+            "description": "This is a invalid tag",
+            "data_type": "float",
             "range_low": 0.2,
             "range_high": 0.8,
         }
@@ -87,6 +111,20 @@ class TagsModelTest(unittest.TestCase):
     def test_invalid_description_missing(self):
         json = {
             "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "data_type": "float",
+            "range_low": 0.2,
+            "range_high": 0.8,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_datatype_missing(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "description": "This is a invalid tag",
             "name": "Valid Tag",
             "range_low": 0.2,
             "range_high": 0.8,
@@ -94,11 +132,141 @@ class TagsModelTest(unittest.TestCase):
         with pytest.raises(ValidationError):
             _ = Tag.model_validate(json)
 
+    def test_invalid_datatype(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "testing",
+            "range_low": 0.2,
+            "range_high": 0.8,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+
+class TagsModelFloat(unittest.TestCase):
+    def test_valid_float(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "float",
+            "range_low": 0.2,
+            "range_high": 0.8,
+        }
+        tag = Tag(
+            id=1,
+            device_id=12,
+            name="Valid Tag",
+            description="This is a valid tag",
+            data_type="float",
+            range_low=0.2,
+            range_high=0.8,
+        )
+        from_json = Tag.model_validate(json)
+        self.assertEqual(tag, from_json)
+
+    def test_invalid_max_length(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "max_length": "8",
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_valid_float_int_range(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "float",
+            "range_low": 2,
+            "range_high": 8,
+        }
+        tag = Tag(
+            id=1,
+            device_id=12,
+            name="Valid Tag",
+            description="This is a valid tag",
+            data_type="float",
+            range_low=2,
+            range_high=8,
+        )
+        from_json = Tag.model_validate(json)
+        self.assertEqual(tag, from_json)
+
+    def test_valid_float_range_low_int(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "float",
+            "range_low": 2,
+            "range_high": 2.8,
+        }
+        tag = Tag(
+            id=1,
+            device_id=12,
+            name="Valid Tag",
+            description="This is a valid tag",
+            data_type="float",
+            range_low=2,
+            range_high=2.8,
+        )
+        from_json = Tag.model_validate(json)
+        self.assertEqual(tag, from_json)
+
+    def test_valid_float_range_high_int(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "float",
+            "range_low": 0.2,
+            "range_high": 8,
+        }
+        tag = Tag(
+            id=1,
+            device_id=12,
+            name="Valid Tag",
+            description="This is a valid tag",
+            data_type="float",
+            range_low=0.2,
+            range_high=8,
+        )
+        from_json = Tag.model_validate(json)
+        self.assertEqual(tag, from_json)
+
+    def test_invalid_range_low_higher_than_range_hi(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "float",
+            "range_low": 0.8,
+            "range_high": 0.2,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
     def test_invalid_range_low_missing(self):
         json = {
             "id": 1,
+            "device_id": 12,
             "name": "Valid Tag",
-            "description": "This is a valid tag",
+            "description": "This is a invalid tag",
+            "data_type": "float",
             "range_high": 0.8,
         }
         with pytest.raises(ValidationError):
@@ -107,8 +275,10 @@ class TagsModelTest(unittest.TestCase):
     def test_invalid_range_low_string(self):
         json = {
             "id": 1,
+            "device_id": 12,
             "name": "Valid Tag",
-            "description": "This is a valid tag",
+            "description": "This is a invalid tag",
+            "data_type": "float",
             "range_low": "hello",
             "range_high": 0.8,
         }
@@ -118,8 +288,10 @@ class TagsModelTest(unittest.TestCase):
     def test_invalid_range_high_missing(self):
         json = {
             "id": 1,
+            "device_id": 12,
             "name": "Valid Tag",
-            "description": "This is a valid tag",
+            "description": "This is a invalid tag",
+            "data_type": "float",
             "range_low": 0.2,
         }
         with pytest.raises(ValidationError):
@@ -128,10 +300,74 @@ class TagsModelTest(unittest.TestCase):
     def test_invalid_range_high_string(self):
         json = {
             "id": 1,
+            "device_id": 12,
             "name": "Valid Tag",
-            "description": "This is a valid tag",
+            "description": "This is a invalid tag",
+            "data_type": "float",
             "range_low": 0.2,
             "range_high": "hello",
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+
+class TagsModelInt(unittest.TestCase):
+    def test_valid_int(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "int",
+            "range_low": 2,
+            "range_high": 8,
+        }
+        tag = Tag(
+            id=1,
+            device_id=12,
+            name="Valid Tag",
+            description="This is a valid tag",
+            data_type="int",
+            range_low=2,
+            range_high=8,
+        )
+        from_json = Tag.model_validate(json)
+        self.assertEqual(tag, from_json)
+
+    def test_invalid_max_length(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "max_length": "8",
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_range_low_float(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "range_low": 0.2,
+            "range_high": "8",
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_range_high_float(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "range_low": 2,
+            "range_high": "0.8",
         }
         with pytest.raises(ValidationError):
             _ = Tag.model_validate(json)
@@ -139,10 +375,156 @@ class TagsModelTest(unittest.TestCase):
     def test_invalid_range_low_higher_than_range_hi(self):
         json = {
             "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "range_low": 8,
+            "range_high": 2,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_range_low_missing(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "range_high": 8,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_range_low_string(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "range_low": "hello",
+            "range_high": 8,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_range_high_string(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "range_low": 2,
+            "range_high": "hello",
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_range_high_missing(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a invalid tag",
+            "data_type": "int",
+            "range_low": 2,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+
+class TagsModelString(unittest.TestCase):
+    def test_valid_string(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
             "name": "Valid Tag",
             "description": "This is a valid tag",
-            "range_low": 0.8,
-            "range_high": 0.2,
+            "data_type": "string",
+            "max_length": 12,
+        }
+        tag = Tag(
+            id=1,
+            device_id=12,
+            name="Valid Tag",
+            description="This is a valid tag",
+            data_type="string",
+            max_length=12,
+        )
+        from_json = Tag.model_validate(json)
+        self.assertEqual(tag, from_json)
+
+    def test_invalid_all(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "string",
+            "max_length": 12,
+            "range_low": 2,
+            "range_high": 2,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_range(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "string",
+            "range_low": 2,
+            "range_high": 2,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+
+class TagsModelBool(unittest.TestCase):
+    def test_valid_string(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "bool",
+        }
+        tag = Tag(
+            id=1,
+            device_id=12,
+            name="Valid Tag",
+            description="This is a valid tag",
+            data_type="bool",
+        )
+        from_json = Tag.model_validate(json)
+        self.assertEqual(tag, from_json)
+
+    def test_invalid_max_length(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "bool",
+            "max_length": 12,
+        }
+        with pytest.raises(ValidationError):
+            _ = Tag.model_validate(json)
+
+    def test_invalid_range(self):
+        json = {
+            "id": 1,
+            "device_id": 12,
+            "name": "Valid Tag",
+            "description": "This is a valid tag",
+            "data_type": "bool",
+            "range_low": 2,
+            "range_high": 2,
         }
         with pytest.raises(ValidationError):
             _ = Tag.model_validate(json)
