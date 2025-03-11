7bbb02e545906efeb5a841ca45e38148a11b6e3b
Tho Ha <4588986+ThoDHa@users.noreply.github.com>
2025-03-11 11:46:02 -0400

    Adding updated History and new Device Models

---
+++ src/models/device.py
@@ -0,0 +1,41 @@
+####################################################################################################
+# Copyright © 2024 Ai-OPs, Inc.
+# All rights reserved.
+# The source code contained herein is protected by copyright law and international treaties.
+# Unauthorized reproduction or distribution of this source code, or any portion of it, may result in
+# severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law.
+# www.ai-op.com
+# www.ai-ops.document360.io/docs/end-user-license-agreement
+####################################################################################################
+
+from pydantic import BaseModel, ConfigDict, field_validator, model_validator
+
+from models.tags import Tag
+
+
+class Device(BaseModel):
+    model_config = ConfigDict(extra="forbid", from_attributes=True)
+    id: int
+    name: str
+    description: str
+    tags: list[Tag] = []
+
+    @field_validator("tags", mode="before")
+    @classmethod
+    def add_device_id(cls, tags, values):
+        if "id" in values.data:
+            device_id = values.data["id"]
+            for tag in tags:
+                if not isinstance(tag, Tag):
+                    if "device_id" not in tag:
+                        tag["device_id"] = device_id
+        return tags
+
+    @model_validator(mode="after")
+    def check_tag_list(self):
+        ids = []
+        for tag in self.tags:
+            if tag.id in ids:
+                raise ValueError()
+            ids.append(tag.id)
+        return self
--- src/models/history.py
+++ src/models/history.py
@@ -10,7 +10,17 @@
 
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field, model_validator
+
+
+class Value(BaseModel):
+    model_config = ConfigDict(
+        extra="forbid",
+        from_attributes=True,
+    )
+
+    value: float
+    timestamp: datetime
 
 
 class History(BaseModel):
@@ -19,42 +29,51 @@ class History(BaseModel):
         from_attributes=True,
     )
     id: int
-    value: float
-    timestamp: datetime
+    values: list[Value] = Field(min_length=1)
+
+    # @model_validator(mode="after")
+    # def check_unique_date_time(self):
+    #     timestamps = []
+    #     for value in self.values:
+    #         if value.timestamp in timestamps:
+    #             raise ValueError()
+    #         timestamps.append(value.timestamp)
+    #     return self
 
 
-class HistoryError(Exception):
-    """
-    General device exception
-    """
-
-
-class HistoryNotFoundError(HistoryError):
-    """Exception raised when attempting to create a history that already exists."""
-
-    def __init__(self, history_id, message="History item not found"):
-        self.history_id = history_id
-        self.message = f"{message}: {history_id}"
-        super().__init__(self.message)
-
-
-class HistoryAlreadyExistError(HistoryError):
-    """Exception raised when attempting to create a history that already exists."""
-
-    def __init__(
-        self, history_id, history_timestamp, message="History item already exists"
-    ):
-        self.history_id = history_id
-        self.history_timestamp = history_timestamp
-        self.message = f"{message}: {history_id} {history_timestamp}"
-        super().__init__(self.message)
-
-
-class HistoryValueOutOfRangeError(HistoryError):
-    def __init__(
-        self, history_id, history_timestamp, message="History value out of range"
-    ):
-        self.history_id = history_id
-        self.history_value = history_value
-        self.message = f"{message}: {history_id} {history_timestamp}"
-        super().__init__(self.message)
+#
+# class HistoryError(Exception):
+#     """
+#     General device exception
+#     """
+#
+#
+# class HistoryNotFoundError(HistoryError):
+#     """Exception raised when attempting to create a history that already exists."""
+#
+#     def __init__(self, history_id, message="History item not found"):
+#         self.history_id = history_id
+#         self.message = f"{message}: {history_id}"
+#         super().__init__(self.message)
+#
+#
+# class HistoryAlreadyExistError(HistoryError):
+#     """Exception raised when attempting to create a history that already exists."""
+#
+#     def __init__(
+#         self, history_id, history_timestamp, message="History item already exists"
+#     ):
+#         self.history_id = history_id
+#         self.history_timestamp = history_timestamp
+#         self.message = f"{message}: {history_id} {history_timestamp}"
+#         super().__init__(self.message)
+#
+#
+# class HistoryValueOutOfRangeError(HistoryError):
+#     def __init__(
+#         self, history_id, history_timestamp, message="History value out of range"
+#     ):
+#         self.history_id = history_id
+#         self.history_value = history_value
+#         self.message = f"{message}: {history_id} {history_timestamp}"
+#         super().__init__(self.message)
