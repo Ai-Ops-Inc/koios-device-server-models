cdd3395619ce0c8e0dd94dd4a7028ee6c40b70fa
Tho Ha <4588986+ThoDHa@users.noreply.github.com>
2025-03-14 11:31:21 -0400

    Adding in the Tags Get Request Model

---
+++ src/models/tags_get_request.py
@@ -0,0 +1,24 @@
+####################################################################################################
+# Copyright © 2024 Ai-OPs, Inc.
+# All rights reserved.
+# The source code contained herein is protected by copyright law and international treaties.
+# Unauthorized reproduction or distribution of this source code, or any portion of it, may result in
+# severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law.
+# www.ai-op.com
+# www.ai-ops.document360.io/docs/end-user-license-agreement
+####################################################################################################
+
+from pydantic import BaseModel, Field, model_validator
+
+
+class TagGetRequest(BaseModel):
+    device_id: int | None = Field(default=None)
+    id: int | None = Field(default=None)
+    skip: int = Field(default=0)
+    limit: int = Field(default=10)
+
+    @model_validator(mode="after")
+    def check_params(self):
+        if self.device_id is not None and self.id is not None:
+            raise ValueError()
+        return self
